6c1a6a673391b1db92249a7b3a9f3c8ae8b0483f
li <418005442@qq.com>
2017-05-12 15:51:45 +0800

    文件打开只用wps office 实现

--- src/android/Open.java
+++ src/android/Open.java
@@ -1,22 +1,21 @@
 package com.disusered;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.MimeTypeMap;
 
+import com.disusered.WpsModel.ClassName;
+import com.disusered.WpsModel.OpenMode;
+import com.disusered.WpsModel.PackageName;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.content.Context;
-import android.net.Uri;
-import android.content.Intent;
-import android.os.Environment;
-import android.provider.ContactsContract;
-import android.webkit.MimeTypeMap;
-import android.content.ActivityNotFoundException;
-import android.os.Build;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Locale;
 
 /**
@@ -25,14 +24,17 @@ import java.util.Locale;
 public class Open extends CordovaPlugin {
 
     public static final String OPEN_ACTION = "open";
-
+    private static final int OPEN_FILE_REQUEST = 1;
+    private CallbackContext callbackContext;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+
         if (action.equals(OPEN_ACTION)) {
             String fileName = args.getString(0);
             String fileId = args.getString(1);
 
-            this.chooseIntent(fileId,fileName, callbackContext);
+            this.chooseIntent(fileId,fileName);
             return true;
         }
         return false;
@@ -63,36 +65,17 @@ public class Open extends CordovaPlugin {
     /**
      * Creates an intent for the data of mime type
      *
-     * @param url             ,file name or full fileName
-     * @param callbackContext
      */
-    private void chooseIntent(String fileId,String fileName, CallbackContext callbackContext) {
-        if (fileId != null && fileId.length() > 0) {
+    private void chooseIntent(String fileId,String fileName) {
+       /* if (fileId != null && fileId.length() > 0) {
             try {
-                //String realPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "huatechTemp" + File.separator + fileId;
-
-                //包含文件分割符，传的是完整路径
-//                if(fileId.contains(File.separator))
-//                {
-//                    //// TODO: 2016/8/13 外部传入的路径格式要做特殊处理才能使用
-//                    realPath =  fileId;
-//                }
-//                // 只有一个文件 名，补全路径
-//                else {
-//                    realPath  = Environment.getExternalStorageDirectory().getPath() + File.separator + "huatechTemp" + File.separator + fileId ;
-//                }
-                //String realPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "huatechTemp" + File.separator + fileId;
-
 
                 String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
                 String realPath = cordova.getActivity().getExternalFilesDir("")  + File.separator + fileId +"."+ suffix;
-
                 File f = new File(realPath);
                 if (!f.exists()) {
                     throw new FileNotFoundException();
                 }
-
-                //  Uri uri = Uri.parse(realPath);
                 Uri uri = Uri.fromFile(new File(realPath));
                 String mime = getMimeType(fileName);
                 Intent fileIntent = new Intent(Intent.ACTION_VIEW);
@@ -103,8 +86,8 @@ public class Open extends CordovaPlugin {
                 } else {
                     fileIntent.setDataAndType(uri, mime);
                 }
-                cordova.getActivity().startActivity(fileIntent);
-                callbackContext.success();
+                cordova.setActivityResultCallback(this);
+                cordova.getActivity().startActivity (fileIntent);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
                 callbackContext.error(0);
@@ -114,6 +97,48 @@ public class Open extends CordovaPlugin {
             }
         } else {
             callbackContext.error(2);
+        }*/
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+        String realPath = cordova.getActivity().getExternalFilesDir("")  + File.separator + fileId +"."+ suffix;
+        openFile(realPath);
+
+    }
+    boolean openFile(String path) {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putString(WpsModel.OPEN_MODE, OpenMode.NORMAL); // 打开模式
+        bundle.putBoolean(WpsModel.SEND_CLOSE_BROAD, true); // 关闭时是否发送广播
+        bundle.putString(WpsModel.THIRD_PACKAGE, cordova.getActivity().getPackageName()); // 第三方应用的包名，用于对改应用合法性的验证
+        bundle.putBoolean(WpsModel.CLEAR_TRACE, true);// 清除打开记录
+        // bundle.putBoolean(CLEAR_FILE, true); //关闭后删除打开文件
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setClassName(PackageName.NORMAL, ClassName.NORMAL);
+
+        File file = new File(path);
+        if (file == null || !file.exists()) {
+            callbackContext.error(0);
+            return false;
+        }
+
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        intent.putExtras(bundle);
+        try {
+            cordova.getActivity().startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            callbackContext.error(1);
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == OPEN_FILE_REQUEST ) {
+
+            callbackContext.success();
         }
     }
 }
