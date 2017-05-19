f728a913d9d3cf1d5a6f63122d2694950f019082
huatech2016 <libo@huatech.net>
2017-05-19 17:49:27 +0800

    Update Open.java

--- src/android/Open.java
+++ src/android/Open.java
@@ -3,7 +3,9 @@ package com.disusered;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.webkit.MimeTypeMap;
 
 import com.disusered.WpsModel.ClassName;
@@ -16,6 +18,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Locale;
 
 /**
@@ -26,6 +29,7 @@ public class Open extends CordovaPlugin {
     public static final String OPEN_ACTION = "open";
     private static final int OPEN_FILE_REQUEST = 1;
     private CallbackContext callbackContext;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
@@ -33,7 +37,7 @@ public class Open extends CordovaPlugin {
         if (action.equals(OPEN_ACTION)) {
             String fileName = args.getString(0);
             String fileId = args.getString(1);
-            this.tryToOpenFile(fileId,fileName);
+            this.tryToOpenFile(fileId, fileName);
             return true;
         }
         return false;
@@ -47,90 +51,46 @@ public class Open extends CordovaPlugin {
      */
     private static String getMimeType(String fileName) {
         String mimeType = null;
-
-        //String extension = MimeTypeMap.getFileExtensionFromUrl(fileName);
         int index = fileName.lastIndexOf(".");
         String extension = fileName.substring(index + 1).toLowerCase(Locale.US);
         if (extension != null) {
             MimeTypeMap mime = MimeTypeMap.getSingleton();
             mimeType = mime.getMimeTypeFromExtension(extension);
         }
-
-        System.out.println("Mime type: " + mimeType);
-
         return mimeType;
     }
 
     /**
      * Creates an intent for the data of mime type
-     *
      */
-    private void tryToOpenFile(String fileId,String fileName) {
-       /* if (fileId != null && fileId.length() > 0) {
-            try {
+    private void tryToOpenFile(String fileId, String fileName) {
 
-                String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
-                String realPath = cordova.getActivity().getExternalFilesDir("")  + File.separator + fileId +"."+ suffix;
-                File f = new File(realPath);
-                if (!f.exists()) {
-                    throw new FileNotFoundException();
-                }
-                Uri uri = Uri.fromFile(new File(realPath));
-                String mime = getMimeType(fileName);
-                Intent fileIntent = new Intent(Intent.ACTION_VIEW);
-                fileIntent.addCategory("android.intent.category.DEFAULT");
-                fileIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                if (Build.VERSION.SDK_INT > 15) {
-                    fileIntent.setDataAndTypeAndNormalize(uri, mime); // API Level 16 -> Android 4.1
-                } else {
-                    fileIntent.setDataAndType(uri, mime);
-                }
-                cordova.setActivityResultCallback(this);
-                cordova.getActivity().startActivity (fileIntent);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                callbackContext.error(0);
-            } catch (ActivityNotFoundException e) {
-                e.printStackTrace();
-                callbackContext.error(1);
-            }
-        } else {
-            callbackContext.error(2);
-        }*/
         String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
-        String realPath = cordova.getActivity().getExternalFilesDir("")  + File.separator + fileId +"."+ suffix;
-        if(isWpsFile(realPath))
-        {
+        String realPath = cordova.getActivity().getExternalFilesDir("") + File.separator + fileId + "." + suffix;
+        if (isWpsFile(realPath)) {
             openWpsFile(realPath);
+        } else {
+            openNotWpsFile(fileId, fileName);
         }
-        else {
-            openNotWpsFile(fileId,fileName);
-        }
-
-
     }
-    private boolean isWpsFile(String path)
-    {
-        if(path !=null)
-        {
-            boolean isDoc = path.endsWith("doc") ||path.endsWith("docx");
-            boolean isExcel = path.endsWith("xls") ||path.endsWith("xlsx");
-            boolean isPpt =  path.endsWith("ppt") ||path.endsWith("pptx");
-            if(isDoc||isExcel||isPpt)
-            {
+
+    private boolean isWpsFile(String path) {
+        if (path != null) {
+            boolean isDoc = path.endsWith("doc") || path.endsWith("docx");
+            boolean isExcel = path.endsWith("xls") || path.endsWith("xlsx");
+            boolean isPpt = path.endsWith("ppt") || path.endsWith("pptx");
+            if (isDoc || isExcel || isPpt) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
-        }
-        else {
+        } else {
             callbackContext.error("文件路径有误");
             return false;
         }
 
     }
+
     boolean openWpsFile(String path) {
         Intent intent = new Intent();
         Bundle bundle = new Bundle();
@@ -162,7 +122,7 @@ public class Open extends CordovaPlugin {
         return true;
     }
 
-    private void openNotWpsFile(String fileId,String fileName) {
+    private void openNotWpsFile(String fileId, String fileName) {
         if (fileId != null && fileId.length() > 0) {
             try {
                 String realPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "huatechTemp" + File.separator + fileId;
@@ -193,12 +153,12 @@ public class Open extends CordovaPlugin {
             }
         }
     }
-}
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == OPEN_FILE_REQUEST ) {
+        if (requestCode == OPEN_FILE_REQUEST) {
 
             callbackContext.success();
         }
